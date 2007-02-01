3e8d6f7850363eb5baa934c6eb14ee03acca8b2a
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-02-01 20:32:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-dots@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/R.cache/R.cache/R/getCacheRootPath.R
@@ -0,0 +1,58 @@
+#########################################################################/**
+# @RdocDefault getCacheRootPath
+#
+# @title "Gets the root path to the file cache directory"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{defaultPath}{The default path.}
+#   \item{...}{Not used.}
+# }
+#
+# \value{
+#   Returns the path as a @character string.
+# }
+#
+# @author
+#
+# \seealso{
+#  @seemethod "setCacheRootPath".
+# }
+#
+# @keyword "programming"
+# @keyword "IO"
+# @keyword "internal"
+#*/######################################################################### 
+setMethodS3("getCacheRootPath", "default", function(defaultPath="~/.Rcache", ...) {
+  # Check for option settings
+  path <- getOption("R.cache:rootPath");
+
+  # Backward compatibility
+  if (is.null(path))
+    path <- getOption("R.cache.path");
+
+  # Check for system environment settings
+  if (is.null(path))
+    path <- Sys.getenv("R_CACHE_PATH");
+
+  # Otherwise, use argument 'path'.
+  if (is.null(path) || nchar(path) == 0)
+    path <- defaultPath;
+
+  path;
+})
+
+
+############################################################################
+# HISTORY:
+# 2007-01-24
+# o Renamed argument 'create' to 'mkdirs'.
+# o Added Rdoc comments.
+# 2005-12-06
+# o Created.
+############################################################################
