4d95df33fe2b6efcf99f3dac3151377ab7a3beb0
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-04 01:19:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-dots@163 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/R.huge/R.huge/R/000.R
+++ trunk/Rpacks/R.huge/R.huge/R/000.R
@@ -3,17 +3,10 @@
 # is named "000.R" (zeros).
 ##############################################################################
 
-if (R.Version()$major < 2) {
-  require(R.oo)    || stop("Could not load package: R.oo");
-  require(R.utils) || throw("Package not found: R.utils");
-  options(dontWarnPkgs=unique(c("R.oo", "R.utils", 
-                                "base", "datasets", "graphics", "grDevices", 
-       "methods", "stats", "utils", "Autoloads", getOption("dontWarnPkgs"))))
-} else {
-  # Is autoload() allowed in R v2.0.0 or higher? According to the help one
-  # should not use require(). These methods are need to load the package.
-  autoload("appendVarArgs", package="R.oo")
-  autoload("hasVarArgs", package="R.oo")
-  autoload("setMethodS3", package="R.oo")
-  autoload("setConstructorS3", package="R.oo")
-}
+# Is autoload() allowed in R v2.0.0 or higher? According to the help one
+# should not use require(). These methods are need to load the package.
+autoload("appendVarArgs", package="R.methodsS3")
+autoload("hasVarArgs", package="R.methodsS3")
+autoload("setMethodS3", package="R.methodsS3")
+autoload("setConstructorS3", package="R.oo")
+
