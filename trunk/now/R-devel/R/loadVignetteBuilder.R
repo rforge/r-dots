96776a53a5f0220f2d1d76091e6e0b31a9ba95cd
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-01 01:20:13 +0000

    STANDALONE: Incorporating DM's loadVignetteBuilder(..., mustWork=TRUE).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-dots@1154 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/now/R-devel/R/loadVignetteBuilder.R
+++ trunk/now/R-devel/R/loadVignetteBuilder.R
@@ -4,19 +4,22 @@ function(pkgdir)
     pkgs <- .get_package_metadata(pkgdir)["VignetteBuilder"]
     if (is.na(pkgs))
         pkgs <- NULL
-    unique(pkgs)
+    else if (length(pkgs) > 0L) {
+        pkgs <- unlist(strsplit(pkgs, ","))
+        pkgs <- gsub('[[:space:]]', '', pkgs)
+    }
+    unique(c(pkgs, "utils"))
 }
 
 
 loadVignetteBuilder <-
-function(pkgdir)
+function(pkgdir, mustwork = TRUE)
 {
     pkgs <- getVignetteBuilder(pkgdir)
-    if (length(pkgs) > 0L) {
-        pkgs <- unlist(strsplit(pkgs, ","))
-        pkgs <- gsub('[[:space:]]', '', pkgs)
-        for (pkg in pkgs)
-    	    loadNamespace(pkg)
+    for (pkg in pkgs) {
+        res <- try(loadNamespace(pkg), silent = TRUE)
+        if (mustwork && inherits(res, "try-error")) 
+            stop(gettextf("vignette builder '%s' not found", pkg), domain = NA)
     }
-    unique(c(pkgs, "utils"))
+    pkgs
 }
