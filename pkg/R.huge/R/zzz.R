1267421fd01774e96bd857149d2fff9e5e77fd26
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-05 22:35:04 +0000

    Version: 0.6.1 [2014-01-05] o Now the 'R.huge' Package object is also available when the   package is only loaded (but not attached). o CLEANUP: Bumped package requirements and no-longer needed   import statements from NAMESPACE.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-dots@1917 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R.huge/R/zzz.R
+++ pkg/R.huge/R/zzz.R
@@ -1,18 +1,20 @@
-# Allows conflicts. For more information, see library() and
-# conflicts() in [R] base.
-.conflicts.OK <- TRUE
+.onLoad <- function(libname, pkgname) {
+  ns <- getNamespace(pkgname);
+  pkg <- Package(pkgname);
+  assign(pkgname, pkg, envir=ns);
+} # .onLoad()
 
 
 .onAttach <- function(libname, pkgname) {
-  pkg <- Package(pkgname);
-  assign(pkgname, pkg, pos=getPosition(pkg));
-
+  pkg <- get(pkgname, envir=getNamespace(pkgname));
   startupMessage(pkg);
 }
 
 
 ############################################################################
 # HISTORY:
+# 2013-01-05
+# o Added .onLoad().
 # 2011-07-23
 # o Added a namespace to the package.
 ############################################################################
